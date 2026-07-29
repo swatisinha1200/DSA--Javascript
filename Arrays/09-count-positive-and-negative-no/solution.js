94df5ce8d5e20ad8a72308d8a17be8525e0e46bf
swati sinha <swatisinha1200@gmail.com>
2026-07-29 10:27:04 +0530

    feat:update in count-positive and negative no

--- Arrays/09-count-positive-and-negative-no/solution.js
+++ Arrays/09-count-positive-and-negative-no/solution.js
@@ -1,6 +1,7 @@
 function positiveNegativeNo(arr) {
   let positive = 0;
   let negative = 0;
+  let zero = 0;
   if (arr.length === 0) {
     return "array is empty";
   }
@@ -11,9 +12,9 @@ function positiveNegativeNo(arr) {
     } else if (arr[i] < 0) {
       negative++;
     } else {
-      console.log("number is zero ");
+      zero++;
     }
   }
-  return `Positive number: ${positive}, Negative number: ${negative}`;
+  return `Positive number: ${positive}, Negative number: ${negative} and zero :${zero}`;
 }
-console.log(positiveNegativeNo([10, 20, -6, 15]));
+console.log(positiveNegativeNo([10, 20, -6, 15, 0]));
